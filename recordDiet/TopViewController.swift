680a29bcd0a19fa6dae8241f0285bd639e7aa0df
kazukiomori <kazuki1110170501@gmail.com>
2022-12-19 00:46:21 +0900

    topViewControllerのUI作成

--- recordDiet/TopViewController.swift
+++ recordDiet/TopViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class TopViewController: UIViewController {
-
+    
+    @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet weak var chartView: UIView!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
